e9f3763b38a4cf15f1a11eac008c1c118b7585d2
MG <xlenz7@gmail.com>
2014-07-14 18:49:56 +0300

    get all envs

--- src/routes.js
+++ src/routes.js
@@ -1,15 +1,17 @@
 'use strict';
 
 var self = this;
-var api, html;
+var jobStatus, envs, html;
 
 module.exports = function (cfg, statusSync) {
-    api = require('./controllers/api')(cfg, statusSync);
+    jobStatus = require('./controllers/status')(cfg, statusSync);
+    envs = require('./controllers/envs')(cfg);
     html = require('./controllers/html')(cfg);
     return self;
 };
 
 exports.routes = function (app) {
-    app.get('/status/:job', api.status);
+    app.get('/status/:job', jobStatus.status);
+    app.get('/environments', envs.envs);
     app.get('/', html.appHtml);
 };
