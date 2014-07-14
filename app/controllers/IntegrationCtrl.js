4cc779fe38a0a6f95f06f6535c47654b8c8bdd95
mgrybyk <xlenz7@gmail.com>
2014-07-15 00:35:26 +0300

    moving some files, minor fixes

--- app/controllers/IntegrationCtrl.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-(function () {
-    var app = angular.module('rlmDashboard');
-
-    app.controller('IntegrationCtrl', function ($scope, ActiveTab) {
-        ActiveTab.set(1);
-    });
-})();
