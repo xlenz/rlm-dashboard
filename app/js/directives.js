07a1b204ea9dbb22743d7044638dcc7a39c4c884
MG <xlenz7@gmail.com>
2015-08-26 17:38:17 +0300

    ssm/rlm filter, added ssm envs, added db hostname
    other related changes

--- app/js/directives.js
+++ app/js/directives.js
@@ -1,30 +1,44 @@
 'use strict';
 
-(function () {
+(function() {
     var app = angular.module('ciDashboard');
-    app.directive('tabManager', function () {
+    app.directive('tabManager', function() {
         return {
             restrict: 'E',
             templateUrl: '../view/tabManager.html'
         };
     });
 
-    app.directive('customPopover', function () {
+    app.directive('customPopover', function() {
         return {
             restrict: 'A',
             template: '<button>{{label}}</button>',
-            link: function (scope, el, attrs) {
+            link: function(scope, el, attrs) {
                 scope.label = attrs.popoverLabel;
                 $(el).popover({
                     trigger: 'click',
                     html: true,
-                    content: function () {
+                    content: function() {
                         return $(this).parent().find('.content').html();
                     },
                     placement: attrs.popoverPlacement
                 });
             }
         };
+    });
+
+    app.filter('objectByKeyValFilter', function() {
+        return function(input, filterKey, filterVal) {
+            var filteredInput = {};
+            if (!filterVal) return input;
+            angular.forEach(input, function(value, key) {
+                //exact match only!
+                if (value[filterKey] && value[filterKey] === filterVal) {
+                    filteredInput[key] = value;
+                }
+            });
+            return filteredInput;
+        }
     });
 
 })();
--- config/config.js
+++ config/config.js
@@ -13,6 +13,9 @@ module.exports = function () {
         pageNotFound: pathToApp + '/404.html',
 
         environments: {
+
+// === RLM ===
+
             'QA_RLM_AUTOCONFIG_stl-qa-oalmt1__MAIN': {
                 name: 'stl-qa-oalmt1',
                 sbmType: 'On Premise',
@@ -65,19 +68,23 @@ module.exports = function () {
                     type: 'MSSQL 2012 SP1'
                 }
             },
-            'QA_RLM_AUTOCONFIG_stl-alms-tst4-OnDemand__MAIN': {
-                name: 'stl-alms-tst4',
-                sbmType: 'On Demand',
-                os: 'Server 2008 R2 SE SP1 x64',
-                sol: {
-                    installType: 'Clean',
-                    upgrade: null
-                },
-                database: {
-                    hostname: 'stl-alms-tst4db',
-                    type: 'MSSQL 2012 SP1'
-                }
-            },
+
+            // commented this one to avoid confusions...
+
+            // 'QA_RLM_AUTOCONFIG_stl-alms-tst4-OnDemand__MAIN': {
+            //     name: 'stl-alms-tst4',
+            //     sbmType: 'On Demand',
+            //     os: 'Server 2008 R2 SE SP1 x64',
+            //     sol: {
+            //         installType: 'Clean',
+            //         upgrade: null
+            //     },
+            //     database: {
+            //         hostname: 'stl-alms-tst4db',
+            //         type: 'MSSQL 2012 SP1'
+            //     }
+            // },
+
             'QA_RLM_AUTOCONFIG_stmsqadv-tdb801__MAIN': {
                 name: 'stmsqadv-tdb801',
                 sbmType: 'On Demand',
@@ -91,17 +98,72 @@ module.exports = function () {
                     type: 'MSSQL 2012 SP1'
                 }
             },
+
+// === SSM ===
+
             'QA_SSM_AUTOCONFIG_stl-alms-tst7__MAIN': {
                 name: 'stl-alms-tst7',
                 sbmType: 'On Premise',
                 os: 'Server 2012 R2 SE x64',
                 sol: {
-                    installType: 'Upgrade',
-                    upgrade: '4.5'
+                    installType: 'Clean',
+                    upgrade: null
                 },
                 database: {
-                    hostname: 'localhost',
-                    type: 'MSSQL 2008 R2 SP2'
+                    hostname: 'stl-alms-tst7db',
+                    type: 'Oracle 12'
+                }
+            },
+            'QA_SSM_AUTOCONFIG_stl-qa-oalmt2__MAIN': {
+                name: 'stl-qa-oalmt2',
+                sbmType: 'On Premise',
+                os: 'Server 2008 R2 EE SP1 x64',
+                sol: {
+                    installType: 'Upgrade',
+                    upgrade: '5.0.1->5.1.1->5.2'
+                },
+                database: {
+                    hostname: 'stl-qa-oalmtfs',
+                    type: 'Oracle 11.2.0.3',
+                }
+            },
+            'QA_SSM_AUTOCONFIG_stl-qa-oalmt3__MAIN': {
+                name: 'stl-qa-oalmt3',
+                sbmType: 'On Premise',
+                os: 'Server 2012 SE x64',
+                sol: {
+                    installType: 'Clean',
+                    upgrade: null
+                },
+                database: {
+                    hostname: 't3db',
+                    type: 'MSSQL 2014 SP1'
+                }
+            },
+            'QA_SSM_AUTOCONFIG_stl-alms-tst4__MAIN': {
+                name: 'stl-alms-tst4',
+                sbmType: 'On Demand',
+                os: 'Server 2008 R2 SE SP1 x64',
+                sol: {
+                    installType: 'Clean',
+                    upgrade: null
+                },
+                database: {
+                    hostname: 'stl-alms-tst4db',
+                    type: 'MSSQL 2012 SP1'
+                }
+            },
+            'QA_SSM_AUTOCONFIG_stmsqadv-tdb801__MAIN': {
+                name: 'stmsqadv-tdb801',
+                sbmType: 'On Demand',
+                os: 'Server 2008 R2 EE SP1 x64',
+                sol: {
+                    installType: 'Upgrade',
+                    upgrade: '5.1.1'
+                },
+                database: {
+                    hostname: 'stmsqadv-tdb802',
+                    type: 'MSSQL 2012 SP1'
                 }
             }
         },
@@ -126,8 +188,8 @@ module.exports = function () {
             ispitkovskyi: 'Igor Spitkovskyi',
             omykhaliov: 'Oleksiy Mykhaliov',
             mgrybyk: 'Mykola Grybyk',
-            nlugova: 'Nata Lugova',
-            smuzychenko: 'Serhiy Muzychenko',
+            //newcomer1: 'newcomer1',
+            //newcomer2: 'newcomer2',
             ytymchenko: 'Yulia Tymchenko',
             vmedvedev: 'Vadym Medvedev',
             ydmytriev: 'Yuri Dmytriev',
